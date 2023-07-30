38dcb75ad62934b242f2aadda74a8432bc704a22
Anil Phiyak <phiyakanil@gmail.com>
2023-07-30 15:02:09 +0530

    June session

---
+++ June/A.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ld long double
+#define mp make_pair
+#define pb push_back
+#define mod 1000000007
+#define f first
+#define s second
+#define fastread ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+const int N=2e5+5;
+const int maxn=1002;
+
+void solve(int n,int m)
+{
+    cout<<"hello"<<endl;
+}
+
+int main()
+{ 
+    fastread;
+    #ifndef ONLINEJUDGE
+       clock_t tStart = clock();
+       freopen("input.txt","r",stdin); //can need to change file . this one for taking input
+       freopen("output.txt","w",stdout); // this one for output
+    #endif
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+        solve(n,m);
+    }
+    return 0;
+}
+
+// Observation-Approach
+// constraints:
+// Target: 
+
+
+// TEST CASE 3:
+// TEST CASE 2:
+// TEST CASE 1:
+
+ 
